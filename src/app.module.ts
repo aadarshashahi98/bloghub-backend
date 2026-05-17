03761ad61a29c9664d95ad491bdf131c81789cc8
Aadarsha Shahi <aadarsha.shahi.1000@gmail.com>
2026-05-17 16:29:45 +0545

    sync false

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ dotenv.config();
       database: process.env.DB_DATABASE,
       charset: 'utf8mb4',
       entities: [],
-      synchronize: true,
+      synchronize: false,
       autoLoadEntities: true,
     }),
     ThemeModule,
