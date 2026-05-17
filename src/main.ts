56a23afecf1de41a45f9af27e9d05577aa9d54b3
Aadarsha Shahi <aadarsha.shahi.1000@gmail.com>
2026-05-17 17:37:48 +0545

    CORS Update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:3000", // your Next.js port
+    origin: "http://bloghub-silk.vercel.app/", // your Next.js port
     credentials: true,
   });
 
